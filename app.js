aff6c1545a5e92079e975b8e612f03a7b806c56b
dlghgus5656 <dlghgus5656@gmail.com>
2022-10-09 19:28:47 +0900

    beginPath()
     beginPath()는 새 경로의 context를 만들기위해 사용
    
    사실 사각형을 그리려면 기본적인 단계들을 하나씩 거쳐야 함
    - 사각형 선 그리기: ctx.rect(x, y, w, h);
    - 선의 색이 적용되지 않아서 보이지 않는다
    -> 다음 줄에 ctx.stroke() / ctx.fill() 해서 테두리만 그리거나 채울 수 있음
    - 끊어가기를 원하는 곳 맨 앞에 ctx.beginPath();추가해 새 경로 만들기.
    
    *요약
    - canvas에서 그림을 그릴 때는 단계별로 진행 필요
    - 그린 그림들의 경로를 나눌 수 있음
    -> 새 경로 시작하기: beginPath()

--- app.js
+++ app.js
@@ -10,7 +10,24 @@ canvas.width = 800;
 canvas.height = 800;
 
 // 채워지게
-ctx.fillRect(100, 100, 100, 200);
+// ctx.fillRect(100, 100, 100, 200);
 
 // 비어지게
-ctx.strokeRect(200, 100, 100, 200);
+// ctx.strokeRect(200, 100, 100, 200);
+
+// 선을 그린다.
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+// 색을 채운다.
+ctx.fill();
+
+// 기존 경로와 다른 경로를 생성한다.
+ctx.beginPath();
+// 선을 그린다.
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+// 채울 색을 설정한다.
+ctx.fillStyle = "red";
+// 색을 채운다.
+ctx.fill();
